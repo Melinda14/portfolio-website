4aedc98ade5fb01ebb879f01e863b7a4435c5e32
Melinda14 <78474381+Melinda14@users.noreply.github.com>
2021-02-24 22:09:45 +0100

    Changed scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
 console.log("Hi! Welcome to My Portfolio Site")
-function myFunction() {
-  var x=document.getElementById('myNavtoggle');
-  if (x.className==='navtoggle'){
-    x.className +='responsive';
-  } else {
-    x.className='navtoggle';
-  }
-}
 function menuToggle() {
   var x=document.getElementById('myNavtoggle');
   if (x.className==='navtoggle'){
-    x.className +='responsive';
+    x.className +=' responsive';
   } else {
     x.className='navtoggle';
   }
