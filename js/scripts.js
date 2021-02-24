03d744879c0c05db33904b7db161b76ad16b1c8d
Melinda14 <78474381+Melinda14@users.noreply.github.com>
2021-02-24 14:45:26 +0100

    Updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,11 @@ function myFunction() {
     x.className='navtoggle';
   }
 }
+function menuToggle() {
+  var x=document.getElementById('myNavtoggle');
+  if (x.className==='navtoggle'){
+    x.className +='responsive';
+  } else {
+    x.className='navtoggle';
+  }
+}
